7568031234548f6606b1da56e0f6810278e94a90
sabof <sabof@example.com>
2013-01-09 16:33:32 +0000

    cleanup + docs

--- fai.el
+++ fai.el
@@ -1,20 +1,50 @@
-(defvar fai-indent-function 'fai-test-and-indent)
-(defvar fai-indentable-line-p-function (lambda () t))
-(defvar fai-after-change-indentation t)
-(defvar fai-indent-limit 20)
-(defvar fai-timer nil)
+(defvar fai-indent-function 'fai-indent-line-maybe
+  "Function to call after ever change, when")
+(defvar fai-indentable-line-p-function (es-constantly t)
+  "For mode-specifc cusomizations.")
+(defvar fai-after-change-indentation t
+  "Whether to reindent after every change.
+Useful when you want to keep the keymap and cursor repositioning.")
+(defvar fai-indent-limit 20
+  "Maximum number of lines for after-change indentation.")
 
 (es-define-buffer-local-vars
- fai-change-flag nil
- fai-first-keystroke nil)
+ fai-change-flag nil)
 
-(defun fai-test-and-indent ()
-  "Run some basic checks (including whether fai-mode is on),
-and (indent-according-to-mode)."
+(defun fai-indent-line-maybe ()
+  "\(indent-according-to-mode\) when `fai-indentable-line-p-function' returns non-nil."
   (when (and fai-mode
              (not (eq indent-line-function 'insert-tab))
              (funcall fai-indentable-line-p-function))
     (indent-according-to-mode)))
+
+(defun fai-indent-forward ()
+  "Indent current line, and `fai-indent-limit' lines afterwards."
+  (save-excursion
+    (fai-indent-line-maybe)
+    (dotimes (ignore fai-indent-limit)
+      (forward-line)
+      (fai-indent-line-maybe))))
+
+(defun fai-indent-defun ()
+  "Indents current defun, if it is smaller than `fai-indent-limit'.
+Otherwise `fai-indent-forward'."
+  (let (init-pos
+        line-end-distance)
+    (condition-case nil
+        (save-excursion
+          (setq line-end-distance)
+          (beginning-of-defun)
+          (setq init-pos (point))
+          (forward-sexp)
+          (when (> (1+ (- (line-number-at-pos)
+                          (line-number-at-pos init-pos)))
+                   fai-indent-limit)
+            (error "defun too long"))
+          (goto-char init-pos)
+          (indent-pp-sexp))
+      (error (fai-indent-forward)))
+    (fai-correct-position-this)))
 
 (defun fai-indented-yank (&optional dont-indent)
   (interactive)
@@ -34,9 +64,7 @@ and (indent-according-to-mode)."
       (when (bound-and-true-p font-lock-mode)
         (font-lock-fontify-region starting-point (point)))
       (goto-line line)
-      (goto-char (- (line-end-position) end-distance))
-      ;; (fai-test-and-indent)
-      )))
+      (goto-char (- (line-end-position) end-distance)))))
 
 (defun fai-mouse-yank (event &optional dont-indent)
   (interactive "e")
@@ -62,23 +90,14 @@ and (indent-according-to-mode)."
   (if (region-active-p)
       (delete-region (point) (mark))
       ;; The following functionality might overlap;
-      (if (fai-after-last-char-p)
+      (if (>= (point)
+              (es-visible-end-of-line))
           (progn
-            (fai-correct-position-this)
             (delete-region (point) (1+ (line-end-position)))
             (when (and (fixup-whitespace)
                        (not from-backspace))
-              (backward-char))
-            (fai-test-and-indent))
-          (delete-char 1)))
-  (when fai-after-change-indentation
-    (fai-test-and-indent)))
-
-(defun fai-after-last-char-p ()
-  (>= (point) (es-visible-end-of-line)))
-
-(defun fai-before-or-at-indentation-p ()
-  (<= (current-column) (current-indentation)))
+              (backward-char)))
+          (delete-char 1))))
 
 (defun fai-backspace ()
   (interactive)
@@ -88,60 +107,23 @@ and (indent-according-to-mode)."
           (delete-char -1))
         ( (region-active-p)
           (delete-region (point) (mark)))
-        ( (fai-before-or-at-indentation-p)
+        ( (<= (current-column)
+              (current-indentation))
           (previous-logical-line)
           (end-of-line)
-          (fai-delete t)
-          (fai-test-and-indent))
+          (fai-delete t))
         ( (bound-and-true-p paredit-mode)
           (paredit-backward-delete))
-        ( t (backward-delete-char 1)))
-  (when fai-after-change-indentation
-    (fai-test-and-indent)))
+        ( t (backward-delete-char 1))))
 
 (defun fai-open-line ()
   (interactive)
   (let ((was-at-eol (>= (point) (es-visible-end-of-line))))
     (save-excursion
       (newline))
-    (fai-test-and-indent)
     (save-excursion
       (forward-char)
-      (indent-according-to-mode))))
-
-(defun fai-indent-forward ()
-  (save-excursion
-    (fai-test-and-indent)
-    (dotimes (ignore fai-indent-limit)
-      (forward-line)
-      (fai-test-and-indent))))
-
-(defun fai-reindent-defun ()
-  "Currently only works for lisp"
-  (let (init-pos
-        line-end-distance)
-    (condition-case nil
-        (save-excursion
-          (setq line-end-distance)
-          (beginning-of-defun)
-          (setq init-pos (point))
-          (forward-sexp)
-          (when (> (1+ (- (line-number-at-pos)
-                          (line-number-at-pos init-pos)))
-                   fai-indent-limit)
-            (error "defun too long"))
-          (goto-char init-pos)
-          (indent-pp-sexp))
-      (error (fai-indent-forward)))
-    (when (< (point) indentation-beginning)
-      (goto-char indentation-beginning))
-    ;; (fai-correct-position-this)
-    ))
-
-(defun fai-delete-indentation ()
-  (interactive)
-  (delete-indentation)
-  (fai-test-and-indent))
+      (fai-indent-line-maybe))))
 
 (defun* fai-newline-and-indent ()
   (interactive)
@@ -159,97 +141,63 @@ and (indent-according-to-mode)."
   (when (region-active-p)
     (delete-region (point) (mark)))
   (newline)
-  (fai-test-and-indent)
+  (fai-indent-line-maybe)
   (when (memq major-mode '(nxml-mode web-mode))
     (save-excursion
       (forward-line -1)
       (indent-according-to-mode))))
 
-(defun fai-correct-position-this (&optional maybe-change-line)
-  ;; Actually useless without maybe-change-line
-  (let (( init-pos (point))
-        ( indentation-beginning
-          (es-indentation-end-pos)))
-    (if (and maybe-change-line
-             (> indentation-beginning init-pos))
-        (cond ( (memq this-command '(backward-char left-char))
-                (end-of-line 0))
-              ( (memq this-command '(forward-char right-char))
-                (back-to-indentation))
-              ))))
+(defun fai-correct-position-this ()
+  "Go back to indentation, if point is before indentation."
+  (let ((indentation-beginning (es-indentation-end-pos)))
+    (when (< (point) indentation-beginning)
+      (goto-char indentation-beginning))))
 
-(defun fai-before-change-hook (&rest ignore)
+(defun fai-before-change-function (&rest ignore)
   "Change tracking."
-  (when fai-after-change-indentation
-    (setq fai-change-flag t)))
+  (setq fai-change-flag t))
 
 (defun* fai-post-command-hook ()
   "First key stroke tracking, cursor correction"
-  (when (or (not fai-mode))
+  (unless fai-mode
     (return-from fai-post-command-hook))
-  (let ( (last-input-structural
-          (member last-input-event
-                  (mapcar 'string-to-char
-                          (list "(" ")" "[" "]" "{" "}" "," ";" " ")))))
-    (setq fai-first-keystroke
-          (and (eq this-command 'self-insert-command)
-               (or last-input-structural
-                   (not (eq last-command 'self-insert-command)))))
-    (when (and (not (region-active-p))
-               (not cua--rectangle)
-               (memq this-command
-                     '(backward-char forward-char
-                       left-char right-char
-                       previous-line next-line)))
-      (fai-correct-position-this t))
-    (fai-indent-ontimer-embedded)
-    ))
-
-(defun fai-indent-ontimer ()
-  "Tests and indentation"
-  (when (and fai-change-flag
-             (buffer-modified-p)
-             fai-mode
-             (or fai-first-keystroke
-                 (not (memq
-                       last-command
-                       '(save-buffer
-                         delete-horizontal-space
-                         undo
-                         undo-tree-undo
-                         undo-tree-redo
-                         quoted-insert
-                         backward-paragraph
-                         self-insert-command))))
-             (not (region-active-p)))
-    (funcall fai-indent-function)
-    ;; (message "indent")
-    )
-  (setq fai-change-flag nil)
-  )
-
-(defun fai-indent-ontimer-embedded ()
-  "Tests and indentation"
-  (when (and fai-change-flag
-             (buffer-modified-p)
-             fai-mode
-             (or fai-first-keystroke
-                 (not (memq
-                       this-command
-                       '(save-buffer
-                         delete-horizontal-space
-                         undo
-                         undo-tree-undo
-                         undo-tree-redo
-                         quoted-insert
-                         backward-paragraph
-                         self-insert-command))))
-             (not (region-active-p)))
-    (funcall fai-indent-function)
-    ;; (message "indent")
-    )
-  (setq fai-change-flag nil)
-  )
+  (let* (( last-input-structural
+           (member last-input-event
+                   (mapcar 'string-to-char
+                           (list "(" ")" "[" "]" "{" "}" "," ";" " "))))
+         ( first-keystroke
+           (and (eq this-command 'self-insert-command)
+                (or last-input-structural
+                    (not (eq last-command 'self-insert-command))))))
+    ;; Correct position
+    (when (and (es-neither (region-active-p)
+                           (bound-and-true-p cua--rectangle)
+                           (bound-and-true-p multiple-cursors-mode))
+               (> (es-indentation-end-pos) (point)))
+      (cond ( (memq this-command '(backward-char left-char))
+              (end-of-line 0))
+            ( (memq this-command
+                    '(forward-char right-char
+                      previous-line next-line))
+              (back-to-indentation))))
+    ;; It won't indent if corrected
+    (when (and fai-after-change-indentation
+               fai-change-flag
+               (buffer-modified-p)
+               (or first-keystroke
+                   (not (memq
+                         this-command
+                         '(save-buffer
+                           delete-horizontal-space
+                           undo
+                           undo-tree-undo
+                           undo-tree-redo
+                           quoted-insert
+                           backward-paragraph
+                           self-insert-command))))
+               (not (region-active-p)))
+      (funcall fai-indent-function))
+    (setq fai-change-flag nil)))
 
 (defun fai--init ()
   (eval-after-load "multiple-cursors-core"
@@ -266,15 +214,13 @@ and (indent-according-to-mode)."
             (fai-delete)
             (cua-delete-region)))))
   (setq inhibit-modification-hooks nil)
-  (pushnew 'fai-before-change-hook before-change-functions)
+  (pushnew 'fai-before-change-function before-change-functions)
   (add-hook 'post-command-hook 'fai-post-command-hook t t)
-  (setq fai-timer
-        (run-with-idle-timer 0 t 'fai-indent-ontimer))
   (es-define-keys fai-mode-map
     [mouse-2] 'fai-mouse-yank
-    "\C-v" 'fai-indented-yank
+    [remap cua-paste] 'fai-indented-yank
+    [remap yank] 'fai-indented-yank
     [remap newline] 'fai-newline-and-indent
-    [remap delete-indentation] 'fai-delete-indentation
     [remap open-line] 'fai-open-line
     [remap delete-char] 'fai-delete
     [remap forward-delete] 'fai-delete
