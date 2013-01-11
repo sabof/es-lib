856dec7fa2388b7a839dae0bad6e3c3bd4fc4bc5
sabof <sabof@example.com>
2013-01-11 14:59:26 +0000

    comments version -1

--- es-hacks-css-mode.el
+++ es-hacks-css-mode.el
@@ -1,4 +1,3 @@
-(require 'haml-mode)
 
 (defun es-css-comment-line-p ()
   (interactive)
@@ -9,40 +8,58 @@
             '(font-lock-comment-face
               font-lock-comment-delimiter-face))))
 
+(defun es-css-indent-comment ()
+  (let* (pnb-indent
+         pnb-last-char
+         ( pnb
+           (save-excursion
+             (es-goto-previous-non-blank-line)
+             (setq pnb-indent (current-indentation))
+             (setq pnb-last-char
+                   (char-before
+                    (end-of-visible-line)))
+             (buffer-substring
+              (line-beginning-position)
+              (line-end-position))))
+         ( pnb-brackets (count ?} pnb))
+         ( pnb-has-colon (plusp (count ?: pnb)))
+         (base pnb-indent))
+    (cond (;; If the previous line is a comment, indent likewise
+           (save-excursion
+             (es-goto-previous-non-blank-line)
+             (when (es-css-comment-line-p)
+               (setq previous-comment-indent
+                     (current-indentation))))
+           (indent-line-to pnb-indent))
+
+          (;; End of colon block
+           (and (not pnb-has-colon)
+                (equal pnb-last-char ?\;)
+                (zerop pnb-brackets))
+           (indent-to (max 0 (- pnb-indent css-indent-offset))))
+          (
+           (or pnb-has-colon (plusp pnb-brackets))
+           (+ pnb-indent (if (not pnb-has-colon) ))
+           (not (eq (save-excursion
+                      (es-goto-previous-non-blank-line)
+                      (last-character))
+                    ?\} ))
+           (indent-line-to css-indent-offset)))))
+
 (defun* es-css-indent-line ()
   (interactive)
-  ;; (when (and (buffer-file-name)
-  ;;            (string-match-p
-  ;;             "\\.scss$"
-  ;;             (buffer-file-name))
-  ;;            (es-line-matches-p
-  ;;             "^[[:blank:]]*@"))
-  ;;   (haml-indent-line)
-  ;;   (return-from es-css-indent-line))
   (css-indent-line)
   (save-excursion
-    (let (previous-comment-indent)
-      (cond ( (= (line-number-at-pos) 1)
-              (indent-line-to 0))
-            ( (save-excursion
-                (es-goto-previous-non-blank-line)
-                (and (es-line-matches-p ":[ ]\\|:$")
-                     (not (es-css-comment-line-p))
-                     (not (es-line-matches-p "[;({]"))))
-              (add-indentation css-indent-offset))
-            ( (and (es-css-comment-line-p)
-                   (save-excursion
-                     (es-goto-previous-non-blank-line)
-                     (when (es-css-comment-line-p)
-                       (setq previous-comment-indent
-                             (current-indentation)))))
-              (indent-line-to previous-comment-indent))
-            ( (and (es-css-comment-line-p)
-                   (not (eq (save-excursion
-                              (es-goto-previous-non-blank-line)
-                              (last-character))
-                            ?\} )))
-              (indent-line-to css-indent-offset)))))
+
+    (cond ( (= (line-number-at-pos) 1)
+            (indent-line-to 0))
+          ( (es-css-comment-line-p)
+            (es-css-indent-comment))
+          ( (save-excursion
+              (es-goto-previous-non-blank-line)
+              (and (es-line-matches-p ":[ ]\\|:$")
+                   (not (es-line-matches-p "[;({]"))))
+            (add-indentation css-indent-offset))))
   (when (< (current-column) (current-indentation))
     (back-to-indentation)))
 
@@ -59,8 +76,8 @@
 (defun es-css-inside-function ()
   (save-excursion
     (ignore-errors
-      (progn (backward-up-list)
-             t))))
+      (backward-up-list)
+      t)))
 
 (defun* css-end-of-defun (&optional (arg 1))
   (interactive)
