36527442580ffa57b62ce8b43928236e73f6c1f0
sabof <sabof@example.com>
2013-01-11 14:35:13 +0000

    stuff

--- es-hacks-css-mode.el
+++ es-hacks-css-mode.el
@@ -1,3 +1,5 @@
+(require 'haml-mode)
+
 (defun es-css-comment-line-p ()
   (interactive)
   (or (save-excursion
@@ -7,8 +9,16 @@
             '(font-lock-comment-face
               font-lock-comment-delimiter-face))))
 
-(defun es-css-indent-line ()
+(defun* es-css-indent-line ()
   (interactive)
+  ;; (when (and (buffer-file-name)
+  ;;            (string-match-p
+  ;;             "\\.scss$"
+  ;;             (buffer-file-name))
+  ;;            (es-line-matches-p
+  ;;             "^[[:blank:]]*@"))
+  ;;   (haml-indent-line)
+  ;;   (return-from es-css-indent-line))
   (css-indent-line)
   (save-excursion
     (let (previous-comment-indent)
@@ -37,17 +47,32 @@
     (back-to-indentation)))
 
 (defun* css-beginning-of-defun (&optional (arg 1))
-  (when (re-search-backward "^[^\n 	].+{[ ]?$" nil t arg)
+  (when (progn
+          (unless (zerop (current-column))
+            (end-of-line))
+          (re-search-backward "^[^\n 	].+{[ ]?$" nil t arg))
     (while (save-excursion
              (forward-line -1)
              (es-line-matches-p "^[^}[:space:]/]"))
       (forward-line -1))))
 
+(defun es-css-inside-function ()
+  (save-excursion
+    (ignore-errors
+      (progn (backward-up-list)
+             t))))
+
 (defun* css-end-of-defun (&optional (arg 1))
   (interactive)
-  (when (re-search-forward "}" nil t arg)
-    (ignore-errors
-      (forward-char))
+  (ignore-errors
+    (if (es-css-inside-function)
+        (css-beginning-of-defun))
+    (progn
+      (re-search-forward "{" nil t arg)
+      (backward-char)
+      (forward-sexp)
+      (ignore-errors
+        (forward-char)))
     t))
 
 (defun es-hacks-css-mode-setup ()
@@ -57,6 +82,14 @@
   (set (make-local-variable 'end-of-defun-function)
        'css-end-of-defun)
   (set (make-local-variable 'es-aai-indent-function)
-       'es-aai-indent-defun))
+       'es-aai-indent-defun)
+  ;; (when (and (buffer-file-name)
+  ;;            (string-match-p
+  ;;             "\\.scss$"
+  ;;             (buffer-file-name)))
+  ;;   (set (make-local-variable 'es-aai-indentable-line-p-function)
+  ;;        (lambda ()
+  ;;          (not (es-line-matches-p "^[[:blank:]]*@")))))
+  )
 
 (provide 'es-hacks-css-mode)
